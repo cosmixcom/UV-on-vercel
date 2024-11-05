974335b35a203a1a31ca55d79764183968f7e4bc
Zak Boem <63625087+cosmixcom@users.noreply.github.com>
2024-11-04 17:20:05 -0700

    Update example.js

--- public/example.js
+++ public/example.js
@@ -7,7 +7,7 @@
 //         }
 //     });
 
-document.getElementById("form").onsubmit = function (event) {
+document.getElementById("form").addEventListener('submit', (event) => {
     event.preventDefault();
 
     let url = document.getElementById("urlInput").value; // if no periods are detected in the input, search google instead
